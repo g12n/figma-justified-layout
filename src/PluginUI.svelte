8d1f060455362e227e5ba91d9b99df60d5c89cec
mge <mge@pixum.com>
2020-05-08 23:17:15 +0200

    rudimetary UI and functionality

--- src/PluginUI.svelte
+++ src/PluginUI.svelte
@@ -5,7 +5,7 @@
 	import { GlobalCSS } from 'figma-plugin-ds-svelte';
 
 	//import some Svelte Figma UI components
-	import { Button, Input, Label, SelectMenu } from 'figma-plugin-ds-svelte';
+	import { Button, Input, Label,Section, SelectMenu, Type,IconSpacing,IconHorizontalPadding } from 'figma-plugin-ds-svelte';
 
 	//menu items, this is an array of objects to populate to our select menus
 	let menuItems = [
@@ -13,20 +13,43 @@
         { 'value': 'triangle', 'label': 'Triangle ', 'group': null, 'selected': false },
         { 'value': 'circle', 'label': 'Circle', 'group': null, 'selected': false }
 	];
+	
+	const settings = {
+		targetRowHeight: 10,
+		layout: "JUSTIFIED",
+		containerPadding: 10,
+		boxSpacing: 10
+	}
+
 
 	var disabled = true;
 	var selectedShape;
 	var count = 5;
 
+	var nodes  = [];
+
+	window.onmessage = async (event) => {
+ 		nodes = []
+		event.data.pluginMessage.map(node =>{	
+			node.settings = {
+				targetRowHeight: 200,
+				layout: "JUSTIFIED",
+				containerPadding: 20,
+				boxSpacing: 20
+			}
+			nodes.push(node)
+		})
+	}
+
+
 	//this is a reactive variable that will return false when a value is selected from
 	//the select menu, its value is bound to the primary buttons disabled prop
 	$: disabled = selectedShape === null;
 
-	function createShapes() {
+	function updateLayout() {
 		parent.postMessage({ pluginMessage: { 
-			'type': 'create-shapes', 
-			'count': count,
-			'shape': selectedShape.value
+			'type': 'update-layout', 
+			'nodes': nodes,
 		} }, '*');
 	}
 
@@ -38,23 +61,33 @@
 
 
 <div class="wrapper p-xxsmall">
-
-	<Label>Shape</Label>
-	<SelectMenu bind:menuItems={menuItems} bind:value={selectedShape} class="mb-xxsmall"/>
-	
-	<Label>Count</Label>
-	<Input iconText="#" bind:value={count} class="mb-xxsmall"/>
-
+	{#if nodes.length > 0}
+	{#each nodes as node}
+		<div class="frame">
+			<Section>{node.id}</Section>
+			<div class="frame-inputs">
+			<Input iconText="H" bind:value={node.settings.targetRowHeight} class="mb-xxsmall"/>
+			<Input iconName="{IconHorizontalPadding}" bind:value={node.settings.containerPadding} class="mb-xxsmall"/>
+			<Input iconName="{IconSpacing}" bind:value={node.settings.boxSpacing} class="mb-xxsmall"/>
+			</div>
+		</div>
+	{/each}
 	<div class="flex p-xxsmall mb-xsmall">
 	<Button on:click={cancel} variant="secondary" class="mr-xsmall">Cancel</Button>
-	<Button on:click={createShapes} bind:disabled={disabled}>Create shapes</Button>
+	<Button on:click={updateLayout} bind:disabled={disabled}>Update Layout</Button>
 	</div>
-
+	{:else}
+	<Type size="large" weight="bold">Please sellect at least one frame</Type>
+	{/if}
 </div>
 
 
 <style>
 
-/* Add additional global or scoped styles here */
+.frame-inputs{
+	display: grid;
+	grid-template-columns: repeat(3,1fr);
+	grid-gap: var(--size-xxsmall);
+}
 
 </style>
--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,65 @@
+//import justifiedImageGrid from "./justified-image-grid";
+//import * as layoutGeometry from "justified-layout";
+
+import justifiedLayout from './lib/justified-layout';
+
+
+function justifiedImageGrid(frame: ComponentNode | FrameNode) {
+	const children = frame.children.slice(0).reverse();
+	const ratios = [];
+	children.map((child, index) => {
+	  if ("x" in child) {
+		let ratio = child.width / child.height;
+		if (child.getPluginData("justifiedRatio") != "") {
+		  ratio = parseFloat(child.getPluginData("justifiedRatio"));
+		}
+		ratios.push(ratio);
+	  }
+	});
+  
+	let conf = { containerWidth: 100 };
+  
+	if (frame.getPluginData("justifiedImageGridConf") != "") {
+	  conf = JSON.parse(frame.getPluginData("justifiedImageGridConf"));
+	}
+	conf.containerWidth = frame.width;
+	const layout = justifiedLayout(ratios, conf);
+  
+	children.map((child, index) => {
+	  if ("x" in child) {
+		const { top, left, width, height } = layout.boxes[index];
+		child.x = left;
+		child.y = top;
+		child.resize(width, height);
+	  }
+	});
+  
+	frame.resizeWithoutConstraints(frame.width, layout.containerHeight);
+  }
+
+
+
+
+  interface JustifiedLayout {
+	targetRowHeight: number;
+	layout: string;
+	containerPadding: number;
+	boxSpacing: number;
+  }
+  
+  const defaultLayout: JustifiedLayout = {
+	targetRowHeight: 10,
+	layout: "JUSTIFIED",
+	containerPadding: 10,
+	boxSpacing: 10
+  };
+  
+
+
+
+
+
+
 // This plugin will open a modal to prompt the user to enter a number, and
 // it will then create that many rectangles on the screen.
 
@@ -7,6 +69,12 @@
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__, {width: 232, height: 208 });
+figma.ui.postMessage(figma.currentPage.selection)
+
+
+figma.on("selectionchange", () => {
+	figma.ui.postMessage(figma.currentPage.selection)
+})
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
@@ -14,33 +82,25 @@ figma.showUI(__html__, {width: 232, height: 208 });
 figma.ui.onmessage = msg => {
 	// One way of distinguishing between different types of messages sent from
 	// your HTML page is to use an object with a "type" property like this.
-	if (msg.type === 'create-shapes') {
+	if (msg.type === 'update-layout') {
 
-		const nodes: SceneNode[] = [];
+		msg.nodes.map(node =>{
+			
+			let frameNode = figma.getNodeById(node.id)
+			if (frameNode.type === "FRAME" || node.type === "COMPONENT") {
+				frameNode.setPluginData("justifiedImageGridConf", JSON.stringify(node.settings) )
+				justifiedImageGrid(frameNode)
 
-		for (let i = 0; i < msg.count; i++) {
-
-			var shape;
-
-			if (msg.shape === 'rectangle') {
-				shape = figma.createRectangle();
-			} else if (msg.shape === 'triangle') {
-				shape = figma.createPolygon();
-			} else {
-				shape = figma.createEllipse();
 			}
 
-			shape.x = i * 150;
-			shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-			figma.currentPage.appendChild(shape);
-			nodes.push(shape);
-		}
 
-		figma.currentPage.selection = nodes;
-		figma.viewport.scrollAndZoomIntoView(nodes);
+		})
+	//	const nodes: SceneNode[] = [];
+	//	figma.currentPage.selection = nodes;
+	//	figma.viewport.scrollAndZoomIntoView(nodes);
 	}
 
 	// Make sure to close the plugin when you're done. Otherwise the plugin will
 	// keep running, which shows the cancel button at the bottom of the screen.
-	figma.closePlugin();
+	//figma.closePlugin();
 };
